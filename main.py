aaa622fbc245fc63d1f72b8414923651daeedb40
Igorajs <igorajs@example.com>
2023-06-25 11:03:48 +0200

    Stworzenie grida informującego.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import datetime as dt
+
 
 bgc = '#35383d'
 win = tk.Tk()
@@ -11,13 +13,30 @@ title = tk.Label(win, text="Weather Checker", bg=bgc, fg='white', font=('Arial',
 title.pack(ipadx=20, ipady=20, pady=(0,120))
 
 frameCenter = tk.Frame(win, bg=bgc)
-inputLabel = tk.Label(frameCenter, text="Search for City", bg=bgc, fg='white', font=('Arial',13))
+inputLabel = tk.Label(frameCenter, text="Search for City:", bg=bgc, fg='white', font=('Arial',13))
 inputLabel.pack()
 entry = tk.Entry(frameCenter, width=30, font=('Arial 15'))
 sbtn = tk.Button(frameCenter, text="Search", bg='#05a11f', fg='#daebdd',font=('Arial 10 bold'))
 entry.pack(fill="x", side='left', padx=10)
 sbtn.pack(fill="x", side='left', ipadx=10)
 frameCenter.pack()
+
+frameInfo = tk.Frame(win, bg=bgc)
+cityName = tk.Label(frameInfo, bg=bgc, font=('Arial 15'), text="*City name*")
+cityName.grid(column=0,row=0,columnspan=6)
+t1 = tk.Label(frameInfo, bg=bgc, font=('Arial 13'), text="Temperatura")
+t1.grid(column=0,row=1)
+w1 = tk.Label(frameInfo, bg=bgc, font=('Arial 13'), text="Wiatr")
+w1.grid(column=1,row=1)
+a1 = tk.Label(frameInfo, bg=bgc, font=('Arial 13'), text="Wiatr")
+a1.grid(column=2,row=1)
+e1 = tk.Label(frameInfo, bg=bgc, font=('Arial 13'), text="Wiatr")
+e1.grid(column=3,row=1)
+y1 = tk.Label(frameInfo, bg=bgc, font=('Arial 13'), text="Wiatr")
+y1.grid(column=4,row=1)
+u1 = tk.Label(frameInfo, bg=bgc, font=('Arial 13'), text="Wiatr")
+u1.grid(column=5,row=1)
+frameInfo.pack()
 
 
 
